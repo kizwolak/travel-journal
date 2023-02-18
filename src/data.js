4733722e8d3fa6696ec67fedebdea204690bd920
kizwolak <ki.zwolak@gmail.com>
2023-02-18 13:05:49 -0300

    Finish project

--- src/data.js
+++ src/data.js
@@ -8,5 +8,23 @@ export default [
         endDate: "??",
         description: "Buenos Aires and the conurbano is where I am based right now. It's a city (or rather, an area!) full of colour, good food, amazing people and starking contrasts. It's the biggest city I've been to and even though driving here is a nightmare, I would not change living here for anything.",
         imageUrl: "https://images.unsplash.com/photo-1612294037637-ec328d0e075e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+    },
+    {
+        title: "Amsterdam",
+        location: "The Netherlands",
+        googleMapsUrl: "https://goo.gl/maps/oqrpPnp3HPudE7NK7",
+        startDate: "1 Sep, 2019",
+        endDate: "intermittently until this day :-)",
+        description: "My most favourite city in Europe. It is so warm and relaxed you feel at home the moment you arrive there. Full of history, art, culture and good food (do not underestimate kapsalon and herring in June!) Highly recommended.",
+        imageUrl: "https://images.unsplash.com/photo-1583295125721-766a0088cd3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80"
+    }, 
+    {
+        title: "Heidelberg",
+        location: "Germany",
+        googleMapsUrl: "https://goo.gl/maps/czF7j6zb45BvJMy9A",
+        startDate: "1 Dec, 2019",
+        endDate: "3 Dec, 2019",
+        description: "A surprisingly interesting city with an amazing university. Beautiful, fully walkable and known for its amazing wine. A hidden gem! Also close to Frankfurt.",
+        imageUrl: "https://images.unsplash.com/photo-1632660609710-ca2ee0c3425b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
     }
 ]
